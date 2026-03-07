63024575d59b8cd032b6ced1b2d5405a823dd22e
Isaac Sante <43215955+IsaacSante@users.noreply.github.com>
2026-03-07 17:26:19 -0500

    get rid of boiler plate code

---
+++ app/about.tsx
@@ -0,0 +1,20 @@
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { StyleSheet } from 'react-native';
+
+export default function AboutScreen() {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">About</ThemedText>
+      <ThemedText>GRINGO — a music player.</ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    paddingTop: 60,
+  },
+});
